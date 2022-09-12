df6e591dde9d85416b71a66e9a4bed430daa2f72
Leo Ruiz <ruizruizleo@hotmail.com>
2022-09-12 14:36:03 -0300

    paginacao

--- src/controllers/produto.js
+++ src/controllers/produto.js
@@ -1,29 +1,64 @@
 const produtoService = require('../services/produto');
 
-const findAllProdutoController = (req, res) => {
-    res.send({ message: 'achado todos com sucesso!' });
+const findAllProdutoController = async (req, res) => {
+  const offset = req.query.offset;
+  const limit = req.query.limit;
+
+  const produtos = await produtoService.findProdutoService(offset, limit);
+  const total = await produtoService.countProdutos();
+
+  const currentUrl = req.baseUrl;
+
+  const next = offset + limit;
+  const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+  const previous = offset - limit < 0 ? null : offset - limit;
+  const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
+  console.log(previousUrl);
+  res.send(produtos);
 };
 
-const findProdutoByIdController = (req, res) => {
-  res.send({ message: 'achado com sucesso!' });
+const findProdutoByIdController = async (req, res) => {
+  try{
+    res.send(await produtoService.findProdutoByIdService(id));
+  } catch (err) {
+    res.status(500).send({ message: "Erro inesperado, tente novamente mais tarde"});
+    console.log(err.message);
+  }
 };
 
-const createProdutoController = (req, res) => {
-  res.send({ message: 'criado com sucesso!' });
+const createProdutoController = async (req, res) => {
+  try{
+    res.send(await produtoService.createProdutoService(req.body));
+  } catch (err) {
+    res.status(500).send({ message: "Erro inesperado, tente novamente mais tarde"});
+    console.log(err.message);
+  }
 };
 
 const updateProdutoController = (req, res) => {
-  res.send({ message: 'atualizado com sucesso!' });
+  try{
+    res.send({ message: 'atualizado com sucesso!' });
+  } catch (err) {
+    res.status(500).send({ message: "Erro inesperado, tente novamente mais tarde"});
+    console.log(err.message);
+  }
 };
 
 const deleteProdutoController = (req, res) => {
+  try{
   res.send({ message: 'deletado com sucesso!' });
+  } catch (err) {
+    res.status(500).send({ message: "Erro inesperado, tente novamente mais tarde"});
+    console.log(err.message);
+  }
 };
 
 module.exports = {
-    findAllProdutoController,
-    findProdutoByIdController,
-    createProdutoController,
-    updateProdutoController,
-    deleteProdutoController
+  findAllProdutoController,
+  findProdutoByIdController,
+  createProdutoController,
+  updateProdutoController,
+  deleteProdutoController
 };
